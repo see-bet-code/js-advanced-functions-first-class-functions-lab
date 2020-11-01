b0fad573e475edcb8dd37a6a4c524ef6f9ea8431
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-01 16:45:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array) { return array.slice(0,2)}
+const returnLastTwoDrivers = function(array) { return array.slice(2) }
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(int) {
+    return function(fare) {
+        return fare * int
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers)
+}
